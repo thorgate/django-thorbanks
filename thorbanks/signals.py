5b8db880793df89514ad949ebef39db016b75504
sirbigg <gotsandriy@gmail.com>
2022-09-27 13:55:45 +0200

    Removed providing_args argument from django.dispatch.Signal due to removal in Django 4.0

--- thorbanks/signals.py
+++ thorbanks/signals.py
@@ -1,10 +1,20 @@
+import django
 import django.dispatch
 
 
-transaction_started = django.dispatch.Signal(providing_args=["transaction"])
-transaction_succeeded = django.dispatch.Signal(providing_args=["transaction"])
-transaction_failed = django.dispatch.Signal(providing_args=["transaction"])
+if django.VERSION >= (4, 0):
+    transaction_started = django.dispatch.Signal()
+    transaction_succeeded = django.dispatch.Signal()
+    transaction_failed = django.dispatch.Signal()
 
-auth_started = django.dispatch.Signal(providing_args=["auth"])
-auth_succeeded = django.dispatch.Signal(providing_args=["auth"])
-auth_failed = django.dispatch.Signal(providing_args=["auth"])
+    auth_started = django.dispatch.Signal()
+    auth_succeeded = django.dispatch.Signal()
+    auth_failed = django.dispatch.Signal()
+else:
+    transaction_started = django.dispatch.Signal(providing_args=["transaction"])
+    transaction_succeeded = django.dispatch.Signal(providing_args=["transaction"])
+    transaction_failed = django.dispatch.Signal(providing_args=["transaction"])
+
+    auth_started = django.dispatch.Signal(providing_args=["auth"])
+    auth_succeeded = django.dispatch.Signal(providing_args=["auth"])
+    auth_failed = django.dispatch.Signal(providing_args=["auth"])
