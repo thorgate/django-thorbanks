e367e029f8e6b96e69f9ba78b7ceaeab163e461a
Dmitri Mussorin <dmitrym@gmx.com>
2023-04-06 14:43:56 +0300

    Fix re_path import

--- thorbanks/urls.py
+++ thorbanks/urls.py
@@ -2,7 +2,7 @@ import django
 from thorbanks.views import response
 
 if django.VERSION >= (4, 0):
-    from django.conf.urls import re_path
+    from django.urls import re_path
 
     urlpatterns = [
         re_path(r"^thorbanks_response/$", response, name="thorbanks_response"),
